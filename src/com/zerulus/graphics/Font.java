fb9a8df05a7c8113a914b82ca30454908442aef9
Zerulus <danielrcastro10@gmail.com>
2017-03-08 10:30:17 -0600

    fixed fonts

--- src/com/zerulus/graphics/Font.java
+++ src/com/zerulus/graphics/Font.java
@@ -1,38 +1,34 @@
 package com.zerulus.graphics;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
 import javax.imageio.ImageIO;
 
 public class Font {
 
     public BufferedImage fontSheet;
-    private final int TILE_SIZE = 16;
     private int w;
     private int h;
 
-    private int c;
-    private int r;
+    private int x;
+    private int y;
+    
+    private int colImage;
+    
+    private HashMap<Character, Integer> hm;
 
     private String rootPath = System.getProperty("user.dir");
 
-    public Font(String file) {
-        w = TILE_SIZE;
-        h = TILE_SIZE;
-        c = 10;
-        r = 10;
-        fontSheet = loadFont(rootPath + file);
-    }
-
-    public Font(String file, int w, int h, int c, int r) {
+    public Font(String file, int w, int h) {
         this.w = w;
         this.h = h;
-
-        this.c = c;
-        this.r = r;
-
+        hm = new HashMap<Character, Integer>();
         fontSheet = loadFont(rootPath + file);
+        
+        colImage = fontSheet.getWidth() / w;
     }
 
     private BufferedImage loadFont(String file) {
@@ -51,43 +47,16 @@ public class Font {
     }
 
     public BufferedImage getFont(char letter) {
-        int a = 0;
-
-        a = letter;
-
-        if(Character.isDigit(letter)) {
-            a = a - 48;
-        }
-
-
-        switch(a) {
-            case 45: a = 91;
-                break;
-            case 46: a = 92;
-                break;
-            case 44: a = 93;
-                break;
-            case 60:a = 94;
-                break;
-            case 62:a = -94;
-                break;
-            case 33:a = 95;
-                break;
-            case 63:a = 96;
-                break;
-        }
-
-        if (a == 94) {
-            return fontSheet.getSubimage( ((a - 65) % c) * w, ((int) ((a - 65) / r)) * h, w / 2, h);
-        } else if (a == -94) {
-            a = 94;
-            return fontSheet.getSubimage( ((a - 65) % c) * w + (w / 2), ((int) ((a - 65) / r)) * h, w / 2, h);
-        } else if(a >= 0 && a <= 9) {
-            return fontSheet.getSubimage(a * w, 6 * h, w, h);
-        } else {
-            return fontSheet.getSubimage( ((a - 65) % c) * w, ((int) ((a - 65) / r)) * h, w, h);
-        }
-
+    	
+    	int value = hm.get(letter);
+    	x = value % colImage;
+    	y = value / colImage;
+    	
+    	return fontSheet.getSubimage(x, y, w, h);
+    }
+    
+    public void addLetter(int c, int r, Character letter) {
+    	hm.put(letter, c + (r * colImage));
     }
 
 
